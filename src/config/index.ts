deb9c9ebf52f69b6c0ea8ebe3960d3eae8721528
alex <gch2008net@163.com>
2024-10-29 11:24:50 +0800

    BUG

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,12 @@
 // WS 10001 API 10002 CHAT 10008 CONFIG 10009
 
-// export const WS_URL = "wss://imapi.51djt.com/msg_gateway";
-// export const API_URL = "https://imapi.51djt.com/api";
-// export const CHAT_URL = "https://imapi.51djt.com/chat";
+export const WS_URL = "wss://imapi.51djt.com/msg_gateway";
+export const API_URL = "https://imapi.51djt.com/api";
+export const CHAT_URL = "https://imapi.51djt.com/chat";
 
-export const WS_URL = "ws://192.168.8.110:10001";
-export const API_URL = "http://192.168.8.110:10002";
-export const CHAT_URL = "http://192.168.8.110:10008";
+// export const WS_URL = "ws://192.168.8.110:10001";
+// export const API_URL = "http://192.168.8.110:10002";
+// export const CHAT_URL = "http://192.168.8.110:10008";
 
 export const getWsUrl = () => localStorage.getItem("wsUrl") || WS_URL;
 export const getApiUrl = () => localStorage.getItem("apiUrl") || API_URL;
@@ -14,7 +14,7 @@ export const getChatUrl = () => localStorage.getItem("chatUrl") || CHAT_URL;
 
 
 
-// export const ENTERPRISE_URL = "https://enterpriseapi.51djt.com";
+export const ENTERPRISE_URL = "https://enterpriseapi.51djt.com";
 
-export const ENTERPRISE_URL = "http://enterprise-admin-dev.51djt.net";
+// export const ENTERPRISE_URL = "http://enterprise-admin-dev.51djt.net";
 export const getEnterPriseUrl = () => localStorage.getItem("enterPriseUrl") || ENTERPRISE_URL;
--- src/pages/chat/queryChat/MessageItem/CustomMessageRender.tsx
+++ src/pages/chat/queryChat/MessageItem/CustomMessageRender.tsx
@@ -5,9 +5,9 @@ import styles from "./message-item-custom.module.scss";
 import clsx from "clsx";
 
 const CustomMessageRender: FC<IMessageItemProps> = ({ message }) => {
-  let content= JSON.parse(message.customElem?.data);
-  if(message.customElem.extension=="position_msg"){
-     return (
+  let content = JSON.parse(message.customElem?.data);
+  if (message.customElem.extension == "position_msg") {
+    return (
 
       <div className={clsx(styles.content)} >
         <div className={clsx(styles.postionMain)} >
@@ -22,46 +22,68 @@ const CustomMessageRender: FC<IMessageItemProps> = ({ message }) => {
         </div>
         <div className={styles.positionlistbox}>
           {/* <img src="/assets/user/user01.jpg" alt=""/> */}
-            <div>点金台职位</div>
+          <div>点金台职位</div>
         </div>
       </div>
-    ); 
+    );
 
-  }else if (message.customElem.extension=="resume_msg"){
+  } else if (message.customElem.extension == "browsePosition_msg") {
+    return (
+
+      <div className={clsx(styles.content)} >
+        <div className={clsx(styles.postionMain)} >
+          <div className={clsx(styles.headbox)} >
+            <img src={content.companyHeadUri} alt="" className={styles.headw} />
+          </div>
+          <div className={styles.positionbox} >
+            <div className={styles.positontype}>{content.education}/{content.jobName}</div>
+            <div className={styles.postionmy}>{content.salary}</div>
+            <div className={styles.positonname}>{content.companyName}</div>
+          </div>
+        </div>
+        <div className={styles.positionlistbox}>
+          {/* <img src="/assets/user/user01.jpg" alt=""/> */}
+          <div>点金台职位</div>
+        </div>
+      </div>
+    );
+
+  }
+  else if (message.customElem.extension == "resume_msg") {
     return (
 
       <div className={styles.jlBox}>
-                 
-      <div className={styles.jlHead}>
-           <img src={content.Path} alt="" className={styles.jlHeadImg}/>
-           <span className={styles.jlHeadName}>{content.RealName}</span> |
-           <span className={styles.jlHeadSex}>{content.Gender == "1" ? "男" : "女"}</span> |
-           <span className={styles.jlHeadAge}>&lt;{content.Age}</span>
-       </div>      
-       <div className={styles.jlMidMY}>{content.Salary}</div>
-       <div className={styles.jlTX}>
-           <span>{content.Position}</span>
-           <span className={styles.jlTXP}>·</span>
-           <span>{content.JobType}</span>
-       </div>
-       <div className={styles.jlInfo}>
-           <span className={styles.jlInfoUser}>{content.Area}</span>
-           <span className={styles.jlInfoUser}>{content.WorkYears}</span>
-           <span className={styles.jlInfoUser}>{content.EducationalBackground}</span>
-       </div>
-       <div className={styles.jlBoot}>
-           {/* <img src="/assets/user/user01.jpg" alt=""/> */}
-           <div>点金台简历</div>
-       </div>
-   </div>
-    ); 
-  }else{
+
+        <div className={styles.jlHead}>
+          <img src={content.Path} alt="" className={styles.jlHeadImg} />
+          <span className={styles.jlHeadName}>{content.RealName}</span> |
+          <span className={styles.jlHeadSex}>{content.Gender == "1" ? "男" : "女"}</span> |
+          <span className={styles.jlHeadAge}>&lt;{content.Age}</span>
+        </div>
+        <div className={styles.jlMidMY}>{content.Salary}</div>
+        <div className={styles.jlTX}>
+          <span>{content.Position}</span>
+          <span className={styles.jlTXP}>·</span>
+          <span>{content.JobType}</span>
+        </div>
+        <div className={styles.jlInfo}>
+          <span className={styles.jlInfoUser}>{content.Area}</span>
+          <span className={styles.jlInfoUser}>{content.WorkYears}</span>
+          <span className={styles.jlInfoUser}>{content.EducationalBackground}</span>
+        </div>
+        <div className={styles.jlBoot}>
+          {/* <img src="/assets/user/user01.jpg" alt=""/> */}
+          <div>点金台简历</div>
+        </div>
+      </div>
+    );
+  } else {
 
     return (
-      <div className={styles.noShow}>web端暂不支持分享商品消息显示</div>
+      <div className={styles.noShow}>web端暂不支持的消息显示</div>
     );
   }
-  
+
 
 
 
